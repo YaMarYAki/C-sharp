137dce3d70e4ed0fa5790f6b8db65b83f107f0a9
YaMarYAki <154043787+YaMarYAki@users.noreply.github.com>
2025-02-18 23:00:10 +0800

    Add files via upload

---
+++ day3/Student/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+
+enum Subject
+{
+    ISiT,
+    SA,
+    Graph3D,
+    AEVM,
+    MO,
+    Web
+}
+
+class Grade
+{
+    public Subject Subject { get; set; }
+    public int Score { get; set; }
+    public DateTime Date { get; set; }
+
+    public Grade(Subject subject, int score)
+    {
+        Subject = subject;
+        Score = score;
+        Date = DateTime.Now;
+    }
+}
+
+class GradeManager
+{
+    private List<Grade> grades = new List<Grade>();
+
+    public void AddGrade()
+    {
+        Console.WriteLine("Выберите предмет (0 - ISiT, 1 - SA, 2 - GRAPH3D, 3 - AEVM, 4 - MO, 5 - WEB): ");
+        if (int.TryParse(Console.ReadLine(), out int subjectIndex) && Enum.IsDefined(typeof(Subject), subjectIndex))
+        {
+            Subject subject = (Subject)subjectIndex;
+            Console.Write("Введите оценку: ");
+            if (int.TryParse(Console.ReadLine(), out int score) && score >= 0 && score <= 5)
+            {
+                grades.Add(new Grade(subject, score));
+                Console.WriteLine("Оценка добавлена");
+            }
+            else
+            {
+                Console.WriteLine("Некорректная оценка");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Некорректный предмет");
+        }
+    }
+
+    public void ViewGrades()
+    {
+        if (grades.Count == 0)
+        {
+            Console.WriteLine("Нет оценок");
+            return;
+        }
+        Console.WriteLine("Список оценок:");
+        for (int i = 0; i < grades.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. Предмет: {grades[i].Subject}, Оценка: {grades[i].Score}, Дата: {grades[i].Date}");
+        }
+    }
+
+    public void RemoveGrade()
+    {
+        Console.Write("Введите номер оценки: ");
+        if (int.TryParse(Console.ReadLine(), out int gradeIndex) && gradeIndex > 0 && gradeIndex <= grades.Count)
+        {
+            grades.RemoveAt(gradeIndex - 1);
+            Console.WriteLine("Оценка удалена");
+        }
+        else
+        {
+            Console.WriteLine("Некорректный номер оценки");
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        GradeManager gradeManager = new GradeManager();
+
+        while (true)
+        {
+            Console.WriteLine("Меню:");
+            Console.WriteLine("1. Добавить оценку");
+            Console.WriteLine("2. Показать оценки");
+            Console.WriteLine("3. Удалить оценку");
+            Console.WriteLine("4. Выход");
+            Console.Write("Выберите пункт: ");
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1": gradeManager.AddGrade(); break;
+                case "2": gradeManager.ViewGrades(); break;
+                case "3": gradeManager.RemoveGrade(); break;
+                case "4": return;
+                default: Console.WriteLine("Ошибка"); break;
+            }
+        }
+    }
+}

05eeb433c55dcb97deb1993a0ff5ef205e08db9e
YaMarYAki <154043787+YaMarYAki@users.noreply.github.com>
2025-02-25 14:36:06 +0800

    Add files via upload

---
+++ async/async/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.WriteLine("Начало обработки оценок...");
+
+        List<Student> students = new List<Student>
+        {
+            new Student("Маркидонов", new List<int> { 5, 4, 3, 5 }),
+            new Student("Федорков", new List<int> { 2, 3, 3, 4 })
+        };
+
+        await Task.Run(() => ProcessGrades(students));
+
+        Console.WriteLine("Обработка завершена.");
+    }
+
+    static void ProcessGrades(List<Student> students)
+    {
+        foreach (var student in students)
+        {
+            Console.WriteLine($"Обрабатываем оценки для {student.LastName}...");
+            Thread.Sleep(10000);
+            double average = student.Grades.Count > 0 ? (double)student.Grades.Sum() / student.Grades.Count : 0;
+            Console.WriteLine($"Средняя оценка {student.LastName}: {average:F2}");
+        }
+    }
+}
+
+class Student
+{
+    public string LastName { get; }
+    public List<int> Grades { get; }
+
+    public Student(string lastName, List<int> grades)
+    {
+        LastName = lastName;
+        Grades = grades;
+    }
+}
---
+++ async/async/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("async")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("async")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("b5064de6-a8d2-4c66-9f7d-3cee1e841796")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
